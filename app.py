b8cb8d161b11e1d66a6408b3a4658ec334d563f6
Brandon <brandon.jaus@gmail.com>
2016-02-11 20:41:22 -0600

    Updates made

--- app.py
+++ app.py
@@ -9,11 +9,23 @@ app = Flask(__name__)
 # use decorators to link the function to a URL
 @app.route('/')
 @app.route('/hello')
-
 # define the view using a function, which returns a string
 def hello_world():
 	return "Hello, World!"
 
+# dynamic route
+@app.route('/test/<search_query>')
+def search(search_query):
+	return search_query
+
+# dynamic route with explicit status codes	
+@app.route('/name/<name>')
+def index(name):
+	if name.lower() == 'brandon':
+		return 'Hello, {}!'.format(name.capitalize()), 200
+	else:
+		return 'Not Found', 404
+
 # start the development server using the run() method
 if __name__ == "__main__":
-	app.run()
+	app.run(debug=True)
