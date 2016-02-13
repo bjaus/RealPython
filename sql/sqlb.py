84e639070246df7bf83dade19362f85ffc95fe79
Brandon <brandon.jaus@gmail.com>
2016-02-12 22:02:21 -0600

    added more data to new.db

--- sql/sqlb.py
+++ sql/sqlb.py
@@ -20,3 +20,14 @@ conn.commit()
 
 # Close the database connection
 conn.close()
+
+# Similar script that doesn't require commit() to be called
+"""
+import sqlite3
+with sqlite3.connect("new.db") as connection:
+	c = connection.cursor()
+	c.execute("INSERT INTO population \
+	VALUES 'New York City', 'NY', 8200000")
+	c.execute("INSERT INTO population \
+	VALUES('San Francisco', 'CA', 8000000)")
+"""
