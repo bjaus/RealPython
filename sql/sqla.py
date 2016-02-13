8ee24d10959c81f0b6142164b1033fd13214f25a
Brandon <brandon.jaus@gmail.com>
2016-02-12 20:54:57 -0600

    database added

--- sql/sqla.py
+++ sql/sqla.py
@@ -1,1 +1,17 @@
-ECHO is on.
+# Create a SQLite3 database and table
+
+# import the sqlite3
+import sqlite3
+
+# create a new database if the database doesn't already exist
+conn = sqlite3.connect('new.db')
+
+# get a cursor object used to execute SQL commands
+cursor = conn.cursor()
+
+# create a table
+cursor.execute("""CREATE TABLE population
+	(city TEXT, state TEXT, population INT)""")
+
+# Close the database connection
+conn.close()
