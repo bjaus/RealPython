f1ac95a02d37146db5618b86e3bb6bc79629fdfc
Brandon <brandon.jaus@gmail.com>
2016-02-12 21:38:25 -0600

    added cars db

--- sql/sqla.py
+++ sql/sqla.py
@@ -4,14 +4,14 @@
 import sqlite3
 
 # create a new database if the database doesn't already exist
-conn = sqlite3.connect('new.db')
+conn = sqlite3.connect('cars.db')
 
 # get a cursor object used to execute SQL commands
 cursor = conn.cursor()
 
 # create a table
-cursor.execute("""CREATE TABLE population
-	(city TEXT, state TEXT, population INT)""")
+cursor.execute("""CREATE TABLE inventory
+	(Make TEXT, Model TEXT, Quantity INT)""")
 
 # Close the database connection
 conn.close()
