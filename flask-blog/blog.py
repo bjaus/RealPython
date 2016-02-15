1cc13f272ba5303928152daa866b891685723f9c
Brandon <brandon.jaus@gmail.com>
2016-02-14 21:13:14 -0600

    current unfound error

--- flask-blog/blog.py
+++ flask-blog/blog.py
@@ -7,6 +7,11 @@ import sqlite3
 
 # configuration
 DATABASE = 'blog.db'
+USERNAME = 'admin'
+PASSWORD = 'admin'
+# Use import os and os.random(24) 
+# to generate SECRET_KEY
+SECRET_KEY = 'hard_to_guess'
 
 app = Flask(__name__)
 
@@ -17,6 +22,31 @@ app.config.from_object(__name__)
 # function used for conecting to the database
 def conenct_db():
 	return sqlite3.conect(app.config['DATABASE'])
+
+@app.route ( '/' , methods = [ 'GET' , 'POST' ]) 
+def login(): 
+	error = None 
+	if request.method == 'POST': 
+		if request.form[ 'username' ] != \
+		app.config[ 'USERNAME' ] or \
+		request.form[ 'password' ] != \
+		app.config[ 'PASSWORD' ]: 
+			error = 'Invalid Credentials. \
+			Please try again.' 
+		else: session[ 'logged_in' ] = True
+		return redirect(url_for( 'main' ))
+		return render_template( 'login.html', \
+			error = error)
+
+@app.route('/logout')
+def logout():
+	session.pop('logged_in', None)
+	flash('You were logged out')
+	return redirect(url_for('login'))
+
+@app.route('/main')
+def main():
+	return render_template('main.html')
 
 if __name__ == '__main__':
 	app.run(debug=True)
