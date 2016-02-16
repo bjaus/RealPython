fa978e867cfd61ce7e6c8f89486961b4deb7f1af
Brandon Jaus <brandon.jaus@gmail.com>
2016-02-16 07:54:49 -0600

    Task app now working

--- flasktaskr/project/views.py
+++ flasktaskr/project/views.py
@@ -5,15 +5,20 @@
 
 import sqlite3
 from functools import wraps
+
 from forms import AddTaskForm
 from flask import (Flask, flash, redirect, render_template,
 				   request, session, url_for, g)
+
 				   
 # config
+
 app = Flask(__name__)
 app.config.from_object('_config')
 
+
 # helper functions
+
 def connect_db():
 	return sqlite3.connect(app.config['DATABASE_PATH'])
 	
@@ -26,8 +31,10 @@ def login_required(test):
 			flash('You need to login first.')
 			return redirect(url_for('login'))
 	return wrap
+
 	
 # route handlers
+
 @app.route('/logout/')
 def logout():
 	session.pop('logged_in', None)
@@ -43,7 +50,7 @@ def login():
 			return render_template('login.html', error=error)
 		else:
 			session['logged_in'] = True
-			flash('Welcome')
+			flash('Welcome!')
 			return redirect(url_for('tasks'))
 	return render_template('login.html')
 	
@@ -52,12 +59,22 @@ def login():
 def tasks():
 	g.db = connect_db()
 	cur = g.db.execute("""
-					SELECT name, due_date, priority, task_id
-					FROM tasks
-					WHERE status=1
-					"""
+		SELECT name, due_date, priority, task_id
+		FROM tasks
+		WHERE status=1
+		"""
 	)
 	open_tasks = [
+		dict(name=row[0], due_date=row[1], priority=row[2], \
+			 task_id=row[3]) for row in cur.fetchall()
+	]
+	cur = g.db.execute("""
+		SELECT name, due_date, priority, task_id
+		FROM tasks
+		WHERE status=0
+		"""
+	)
+	closed_tasks = [
 		dict(name=row[0], due_date=row[1], priority=row[2], \
 			 task_id=row[3]) for row in cur.fetchall()
 	]
@@ -68,6 +85,7 @@ def tasks():
 		open_tasks=open_tasks,
 		closed_tasks=closed_tasks
 	)
+
 	
 # Add new tasks
 @app.route('/add/', methods=['POST'])
@@ -95,6 +113,7 @@ def new_task():
 	g.db.close()
 	flash('New entry was successfully posted. Thanks!')
 	return redirect(url_for('tasks'))
+	
 	
 # Mark tasks as complete
 @app.route('/complete/<int:task_id>/')
