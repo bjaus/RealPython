b4d75d7a2ebcec41c4529e2e65038cf4fa68c136
Brandon Jaus <brandon.jaus@gmail.com>
2016-02-17 08:20:11 -0600

    changed to SQLAlchemy from sqlite3

--- flasktaskr/project/db_create.py
+++ flasktaskr/project/db_create.py
@@ -1,31 +1,17 @@
 # project/db_create.py
 # db_create.py: sets up and creates the database
 
-import sqlite3
-from _config import DATABASE_PATH
+from views import db 
+from models import Task
+from datetime import date 
 
-with sqlite3.connect(DATABASE_PATH) as connection:
 
-	# get a cursor object used to execute SQL commands
-	c = connection.cursor()
-	
-	# create the table
-	c.execute("""
-		CREATE TABLE tasks(
-			task_id INTEGER PRIMARY KEY AUTOINCREMENT,
-			name TEXT NOT NULL,
-			due_date TEXT NOT NULL,
-			priority INTEGER NOT NULL,
-			status INTEGER NOT NULL)
-			""")
-			
-	# insert dummy data into the table
-	c.execute("""
-		INSERT INTO tasks(name, due_date, priority, status)
-		VALUES('Finish this tutorial', '02/15/2016', 10, 1)
-		""")
-		
-	c.execute("""
-		INSERT INTO tasks(name, due_date, priority, status)
-		VALUES('Finish Real Python Course 2', '02/15/2016', 10, 1)
-		""")
+# create the database and the db table
+db.create_all()
+
+# insert database
+db.session.add(Task("Finish this tutorial", date(2016, 2, 20), 10, 1))
+db.session.add(Task("Finish Real Python", date(2016, 4, 20), 10, 1))
+
+# commit the changes
+db.session.commit()
