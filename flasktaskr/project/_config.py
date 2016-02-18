e4f90878c114f433a5c616dfa278e6aa8a97c69c
Brandon Jaus <brandon.jaus@gmail.com>
2016-02-18 08:18:30 -0600

    Reverted back to FlaskTaskr part 1

--- flasktaskr/project/_config.py
+++ flasktaskr/project/_config.py
@@ -3,16 +3,14 @@
 
 import os
 
-
 # grab the folder where this script lives
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 DATABASE = 'flasktaskr.db'
-CSRF_ENABLED = True
+USERNAME = 'admin'
+PASSWORD = 'admin'
+WTF_CSRF_ENABLED = True
 SECRET_KEY = 'my_precious'
 
 # define the full path for the database
 DATABASE_PATH = os.path.join(basedir, DATABASE)
-
-# the database uri
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + DATABASE_PATH
