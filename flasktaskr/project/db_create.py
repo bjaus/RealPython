aea3cd2eccbc9a1f9a71ce00d426bd9a0fa413fe
Brandon <brandon.jaus@gmail.com>
2016-02-21 13:52:02 -0600

    bug caught

--- flasktaskr/project/db_create.py
+++ flasktaskr/project/db_create.py
@@ -9,7 +9,7 @@ db.create_all()
 
 # insert data
 db.session.add(Task('Finish this tutorial', date(2016, 3, 13), 10, 1))
-db.session.add(Task('Finish Real Python', date(2016, 5, 15), 10 1))
+db.session.add(Task('Finish Real Python', date(2016, 5, 15), 10, 1))
 
 # commit the changes
 db.session.commit()
