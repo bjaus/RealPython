e32969cb5e622a538b9db9d1865a69fd567c9f7a
Brandon <brandon.jaus@gmail.com>
2016-02-21 14:58:21 -0600

    data migrated

--- flasktaskr/project/models.py
+++ flasktaskr/project/models.py
@@ -1,6 +1,7 @@
 # project/models.py
 
 from views import db
+import datetime
 
 class Task(db.Model):
 
@@ -10,7 +11,9 @@ class Task(db.Model):
 	name = db.Column(db.String, nullable=False)
 	due_date = db.Column(db.Date, nullable=False)
 	priority = db.Column(db.Integer, nullable=False)
+	posted_date = db.Column(db.Date, default=datetime.datetime.utcnow())
 	status = db.Column(db.Integer)
+	user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
 
 	def __init__(self, name, due_date, priority, status):
 		self.name = name
@@ -29,6 +32,7 @@ class User(db.Model):
 	name = db.Column(db.String, unique=True, nullable=False)
 	email = db.Column(db.String, unique=True, nullable=False)
 	password = db.Column(db.String, nullable=False)
+	tasks = db.relationship('Task', backref='poster')
 
 	def __init__(self, name=None, email=None, password=None):
 		self.name = name
