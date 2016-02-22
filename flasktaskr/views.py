09ecbb3f9ffcacd9ca9da5cb71f27c32bfc1509d
Brandon <brandon.jaus@gmail.com>
2016-02-21 18:53:46 -0600

    added tasks function to views.py

--- flasktaskr/views.py
+++ flasktaskr/views.py
@@ -6,7 +6,7 @@ import sqlite3
 from functools import wraps
 
 from flask import Flask, flash, redirect, render_template, \
-	request, session, url_for
+	request, session, url_for, g
 
 
 # config
@@ -53,3 +53,36 @@ def login():
 			flash('Welcome!')
 			return redirect(url_for('tasks'))
 	return render_template('login.html')
+
+
+@app.route('/tasks/')
+@login_required
+def tasks():
+	g.db = connect_db()
+	cur = g.db.execute("""
+		SLECT name, due_date, priority, task_id
+		FROM tasks
+		WHERE status=1
+		"""
+	)
+	open_tasks = [
+		dict(name=row[0], due_date=row[1], priority=row[2],
+			 task_id=row[3]) for row in cur.fetchall()
+	]
+	cur g.db.execute("""
+		SELECT name, due_date, priority, task_id
+		FROM tasks
+		WHERE status=0
+		"""
+	)
+	closed_tasks = [
+		dict(name=row[0], due_date=row[1], priority=row[2],
+			 task_id=row[3]) for row in cur.fetcall()
+	]
+	g.db.close()
+	return render_template(
+		'tasks.html',
+		form=AddTaskForm(request.form),
+		open_tasks=open_tasks,
+		closed_tasks=closed_tasks
+	)
