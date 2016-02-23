f8d7cee77552d2b7e2e0bbaba2bfcb63e06d1368
Brandon Jaus <brandon.jaus@gmail.com>
2016-02-23 08:01:09 -0600

    binded db tables

--- flasktaskr/models.py
+++ flasktaskr/models.py
@@ -14,6 +14,7 @@ class Task(db.Model):
 	due_date = db.Column(db.Date, nullable=False)
 	priority = db.Column(db.Integer, nullable=False)
 	status = db.Column(db.Integer)
+	user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
 	
 	def __init__(self, name, due_date, priority, status):
 		self.name = name
@@ -33,6 +34,7 @@ class User(db.Model):
 	name = db.Column(db.String, unique=True, nullable=False)
 	email = db.Column(db.String, unique=True, nullable=False)
 	password = db.Column(db.String, nullable=False)
+	tasks = db.relationship('Task', backref='poster')
 	
 	def __init__(self, name=None, email=None, password=None):
 		self.name = name
